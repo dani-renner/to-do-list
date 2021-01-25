bf218123c0281cf737049def99390ef6fe782981
Dani Renner <danijrenner@gmail.com>
2021-01-25 15:51:21 -0800

    add some user interface & styling
    Co-authored-by: Brian Sturgis <sturujisu@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 // business logic
+
+let list = new To_Do_List();
+let list2 = new To_Do_List();
+let list3 = new To_Do_List();
+let list4 = new To_Do_List();
+let entryTasks = new List_Object("hop", "skip", "jump");
+list.addTask(entryTasks);
+
+
+
 function List_Object (tasks){
   this.tasks = [];
   //this.completion = false;
@@ -16,17 +26,22 @@ To_Do_List.prototype.assignId = function() {
   return this.currentId;
 }
 To_Do_List.prototype.markCompletion = function(check){
-
 }
-let list = new To_Do_List();
-let entryTasks = new List_Object("hop", "skip", "jump");
-list.addTask(entryTasks);
-
-
-
-
-
-
-
+// let list = new To_Do_List();
+// let entryTasks = new List_Object("hop", "skip", "jump");
+// list.addTask(entryTasks);
+console.log(list);
+console.log(list2);
+console.log(list3);
+console.log(list4);
+console.log(entryTasks);
 
 // user logic
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const person1Input = $("input#item1").val();
+
+    $(".userEntries").text(person1Input);
+  });
+}):
